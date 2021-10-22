afb83c5a5e6098046c37d0f1c8ee68f8e4afdf9e
Neil Armstrong <gamechanger.astronaut@gmail.com>
2021-10-21 22:07:28 -0700

    login and logout done

--- controller/loginController.js
+++ controller/loginController.js
@@ -71,13 +71,13 @@ async function login(req, res, next) {
 }
 
 // do logout
-// function logout(req, res) {
-//   res.clearCookie(process.env.COOKIE_NAME);
-//   res.send("logged out");
-// }
+function logout(req, res) {
+  res.clearCookie(process.env.COOKIE_NAME);
+  res.send("logged out");
+}
 
 module.exports = {
   getLogin,
   login,
-  //logout,
+  logout,
 };
